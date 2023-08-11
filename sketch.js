eb30931189d428c004d621ddc92c89ac1c69b083
DhiogoLopes <141455193+DhiogoLopes@users.noreply.github.com>
2023-08-11 17:41:19 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(600, 600);
+  background("rgb(26,231,222)
+}
+
+function draw() {
+ 
+  fill("rgb(0,0,0)
+  stroke("rgb(0,0,0)");
+ 
+ 
+  if(mouseIsPressed){
+    rect(mouseX,mouseY,30,40);
+  }
+ 
+ 
+}
